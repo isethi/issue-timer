d03c4479cab961007894311db428ebeb7c8afb9f
Iccha Sethi <isethi@github.com>
2021-04-01 18:48:57 -0500

    Add tests

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module isethi/issue-timer
 go 1.15
 
 require (
+	github.com/google/go-github v17.0.0+incompatible
 	github.com/google/go-github/v33 v33.0.0
 	golang.org/x/oauth2 v0.0.0-20210323180902-22b0adad7558
 )
--- issue_events_test.go
+++ issue_events_test.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"testing"
+	"time"
 
-	"github.com/google/go-github/github"
 	"github.com/google/go-github/v33/github"
+	"golang.org/x/oauth2"
 )
 
 // integration test
@@ -14,7 +16,7 @@ func TestIssuesEvents(t *testing.T) {
 
 	//set up
 
-	githubToken := os.Getenv("INPUT_GITHUB_TOKEN")
+	githubToken := os.Getenv("GITHUB_TOKEN")
 	githubRepository := os.Getenv("GITHUB_REPOSITORY")
 
 	ownerName, repoName := getOwnerAndRepoName(githubRepository)
@@ -22,16 +24,22 @@ func TestIssuesEvents(t *testing.T) {
 	client := getGitHubClient(ctx, githubToken)
 
 	projectName := "1"
+	projectID := int64(1)
 	labelName := "review"
 	timeInColumnStr := "1"
+
+	fmt.Printf("projectName is %s\n", projectName)
+	fmt.Printf("github token is %s\n", githubToken)
+	fmt.Printf("Github Repository is %s\n", githubRepository)
+	fmt.Printf("Time in minutes is %s\n", timeInColumnStr)
+	fmt.Printf("Label Name is %s\n", labelName)
 
 	// create an issue
 
 	input := &github.IssueRequest{
-		Title:    github.String("t"),
-		Body:     github.String("b"),
-		Assignee: github.String("a"),
-		Labels:   &[]string{"l1", "l2"},
+		Title:  github.String("t"),
+		Body:   github.String("b"),
+		Labels: &[]string{"l1", "l2"},
 	}
 
 	iss, _, err := client.Issues.Create(ctx, ownerName, repoName, input)
@@ -42,22 +50,25 @@ func TestIssuesEvents(t *testing.T) {
 
 	// get Project column details
 
-	proj, _, err := client.Projects.GetProject(ctx, 1)
+	projColumns, _, err := client.Projects.ListProjectColumns(ctx, projectID, &github.ListOptions{})
 	if err != nil {
-		t.Errorf("Error getting project: %v", err)
+		t.Errorf("Error getting projec columnst: %v", err)
 	}
 
 	// add it to project board
 
-	input = &github.ProjectCardOptions{
+	projectInput := &github.ProjectCardOptions{
 		ContentID:   iss.GetID(),
 		ContentType: "Issue",
 	}
-	_, _, err = client.Projects.CreateProjectCard(ctx, 1, input)
+	projCard, _, err := client.Projects.CreateProjectCard(ctx, projectID, projectInput)
 	if err != nil {
 		t.Errorf("Error creating project card: %v", err)
 	}
-
+	_, err = client.Projects.MoveProjectCard(ctx, projCard.GetID(), &github.ProjectCardMoveOptions{ColumnID: projColumns[0].GetID()})
+	if err != nil {
+		t.Errorf("Error moving project card: %v", err)
+	}
 	// run fn to make sure label is not added
 
 	ParseIssues(projectName, githubToken, githubRepository, labelName, timeInColumnStr)
@@ -66,30 +77,68 @@ func TestIssuesEvents(t *testing.T) {
 	if err != nil {
 		t.Errorf("Error creating issue: %v", err)
 	}
-	// todo: check if label in issue labels
+
+	labelExists := checkLabelInIssueLabels(iss.Labels, labelName)
+	if labelExists {
+		t.Fatalf("Label should not exist but exists")
+	}
 
 	// sleep for a minute
+	time.Sleep(time.Minute * 1)
 
 	// confirm label is added
-
+	labelExists = checkLabelInIssueLabels(iss.Labels, labelName)
+	if !labelExists {
+		t.Fatalf("Label should  exist but does not exist")
+	}
 	// move issue to another column
 
+	_, err = client.Projects.MoveProjectCard(ctx, projCard.GetID(), &github.ProjectCardMoveOptions{ColumnID: projColumns[1].GetID()})
+	if err != nil {
+		t.Errorf("Error moving project card: %v", err)
+	}
 	// confirm label is removed
+	ParseIssues(projectName, githubToken, githubRepository, labelName, timeInColumnStr)
 
+	labelExists = checkLabelInIssueLabels(iss.Labels, labelName)
+	if labelExists {
+		t.Fatalf("Label should not exist but exists")
+	}
 	//move back to old column
+	_, err = client.Projects.MoveProjectCard(ctx, projCard.GetID(), &github.ProjectCardMoveOptions{ColumnID: projColumns[0].GetID()})
+	if err != nil {
+		t.Errorf("Error moving project card: %v", err)
+	}
 
 	// confirm label is not added back
 
-	// delete issue
+	ParseIssues(projectName, githubToken, githubRepository, labelName, timeInColumnStr)
 
-	// delete board
+	labelExists = checkLabelInIssueLabels(iss.Labels, labelName)
+	if labelExists {
+		t.Fatalf("Label should not exist but exists")
+	}
+
+	// delete issue - no client api to delete issue
+	client.Issues.Edit(ctx, ownerName, repoName, iss.GetNumber(), &github.IssueRequest{State: github.String("closed")})
+	// delete board - no client api to delete board
+
 }
 
-func stringInSlice(a string, list []string) bool {
+func checkLabelInIssueLabels(list []*github.Label, labelName string) bool {
 	for _, b := range list {
-		if b == a {
+		if b.GetName() == labelName {
 			return true
 		}
 	}
 	return false
+}
+
+func getGitHubClient2(ctx context.Context, githubToken string) *github.Client {
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: githubToken},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+	return client
 }
