b92f348c3679ad83e8612311e8ef544abf8f34e1
Iccha Sethi <isethi@github.com>
2021-04-04 20:10:57 -0500

    Update so we dont quit on remove label

--- issue_events.go
+++ issue_events.go
@@ -36,24 +36,23 @@ func ParseIssues(projectName, githubToken, githubRepository, labelName, timeInCo
 		fmt.Printf("Could not list issues in repo. Error: %v\n", err)
 		return
 	}
-	fmt.Printf("issues are %v\n", issues)
+	fmt.Printf("Number of issues in project are %d\n", len(issues))
 
 	// for each issue list issue events
 	for _, issue := range issues {
+		fmt.Printf("Issue : %v\n", issue)
 		events, _, err := client.Issues.ListIssueEvents(ctx, ownerName, repoName, *issue.Number, &github.ListOptions{})
 		if err != nil {
 			fmt.Printf("Could not list events in issue. Error: %v\n", err)
 			return
 		}
-		fmt.Printf("Event: %v\n", events)
-
 		for i := len(events) - 1; i >= 0; i-- {
 			event := events[i]
 			fmt.Printf("Event %d: %s, created: %s\n", i, *event.Event, event.CreatedAt)
 			if event.ProjectCard != nil {
 				fmt.Printf("project column: %s\n", *event.ProjectCard.ColumnName)
 				diff := time.Now().Sub(*event.CreatedAt)
-				fmt.Println(diff)
+				fmt.Printf("Time diff of event: %v\n", diff)
 				if diff.Minutes() > timeInColumn {
 
 					_, _, err := client.Issues.AddLabelsToIssue(ctx, ownerName, repoName, *issue.Number, []string{labelName})
@@ -62,10 +61,10 @@ func ParseIssues(projectName, githubToken, githubRepository, labelName, timeInCo
 						return
 					}
 				} else {
+					// attempt to remove label if it exists
 					_, err := client.Issues.RemoveLabelForIssue(ctx, ownerName, repoName, *issue.Number, labelName)
 					if err != nil {
 						fmt.Printf("Could not remove labels in issue. Error: %v\n", err)
-						return
 					}
 				}
 				break // if we found that an issue has been in a column then we dont parse older events
