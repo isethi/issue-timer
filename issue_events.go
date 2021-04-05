02260eb0d91161552f30f56fdf7c07f664a56a4e
Iccha Sethi <isethi@github.com>
2021-04-04 20:33:14 -0500

    Add list options and pagination

--- issue_events.go
+++ issue_events.go
@@ -31,43 +31,48 @@ func ParseIssues(projectName, githubToken, githubRepository, labelName, timeInCo
 	client := getGitHubClient(ctx, githubToken)
 
 	// list all issues for the repository
-	issues, _, err := client.Issues.ListByRepo(ctx, ownerName, repoName, &github.IssueListByRepoOptions{})
-	if err != nil {
-		fmt.Printf("Could not list issues in repo. Error: %v\n", err)
-		return
-	}
-	fmt.Printf("Number of issues in project are %d\n", len(issues))
-
-	// for each issue list issue events
-	for _, issue := range issues {
-		fmt.Printf("Issue : %v\n", issue)
-		events, _, err := client.Issues.ListIssueEvents(ctx, ownerName, repoName, *issue.Number, &github.ListOptions{})
+	for i := 0; ; i++ {
+		issues, _, err := client.Issues.ListByRepo(ctx, ownerName, repoName, &github.IssueListByRepoOptions{ListOptions: github.ListOptions{Page: i + 1, PerPage: 100}})
 		if err != nil {
-			fmt.Printf("Could not list events in issue. Error: %v\n", err)
+			fmt.Printf("Could not list issues in repo. Error: %v\n", err)
 			return
 		}
-		for i := len(events) - 1; i >= 0; i-- {
-			event := events[i]
-			fmt.Printf("Event %d: %s, created: %s\n", i, *event.Event, event.CreatedAt)
-			if event.ProjectCard != nil {
-				fmt.Printf("project column: %s\n", *event.ProjectCard.ColumnName)
-				diff := time.Now().Sub(*event.CreatedAt)
-				fmt.Printf("Time diff of event: %v\n", diff)
-				if diff.Minutes() > timeInColumn {
+		fmt.Printf("Number of issues in project are %d\n", len(issues))
+		if len(issues) < 1 {
+			break //no more issues
+		}
 
-					_, _, err := client.Issues.AddLabelsToIssue(ctx, ownerName, repoName, *issue.Number, []string{labelName})
-					if err != nil {
-						fmt.Printf("Could not add labels in issue. Error: %v\n", err)
-						return
+		// for each issue list issue events
+		for _, issue := range issues {
+			fmt.Printf("Issue : %v\n", issue)
+			events, _, err := client.Issues.ListIssueEvents(ctx, ownerName, repoName, *issue.Number, &github.ListOptions{})
+			if err != nil {
+				fmt.Printf("Could not list events in issue. Error: %v\n", err)
+				return
+			}
+			for i := len(events) - 1; i >= 0; i-- {
+				event := events[i]
+				fmt.Printf("Event %d: %s, created: %s\n", i, *event.Event, event.CreatedAt)
+				if event.ProjectCard != nil {
+					fmt.Printf("project column: %s\n", *event.ProjectCard.ColumnName)
+					diff := time.Now().Sub(*event.CreatedAt)
+					fmt.Printf("Time diff of event: %v\n", diff)
+					if diff.Minutes() > timeInColumn {
+
+						_, _, err := client.Issues.AddLabelsToIssue(ctx, ownerName, repoName, *issue.Number, []string{labelName})
+						if err != nil {
+							fmt.Printf("Could not add labels in issue. Error: %v\n", err)
+							return
+						}
+					} else {
+						// attempt to remove label if it exists
+						_, err := client.Issues.RemoveLabelForIssue(ctx, ownerName, repoName, *issue.Number, labelName)
+						if err != nil {
+							fmt.Printf("Could not remove labels in issue. Error: %v\n", err)
+						}
 					}
-				} else {
-					// attempt to remove label if it exists
-					_, err := client.Issues.RemoveLabelForIssue(ctx, ownerName, repoName, *issue.Number, labelName)
-					if err != nil {
-						fmt.Printf("Could not remove labels in issue. Error: %v\n", err)
-					}
+					break // if we found that an issue has been in a column then we dont parse older events
 				}
-				break // if we found that an issue has been in a column then we dont parse older events
 			}
 		}
 	}
